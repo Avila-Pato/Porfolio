ef42feec5861c19d1a4af3f09602d56ea352736f
Avila-Pato <152195893+Avila-Pato@users.noreply.github.com>
2024-04-23 00:47:50 -0400

    se agregacosas de mas

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -38,7 +38,7 @@ const MovingImg = ({ title, img, link }) => {
             onMouseMove={handleMouse}
             onMouseLeave={handleMouseleave}
         >
-            <h2 className="capitalize text-xl font-semibold hover:underline">
+            <h2 className="capitalize text-xl font-semibold hover:underline ">
                 {title}
             </h2>
 
@@ -63,35 +63,35 @@ const Article = ({ img, title, date, link }) => {
             // viewport={{ once: true }}
             className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center
         justify-between bg-light text-dark first:mt-0 border border-solid border-dark
-        border-r-4 border-b-4"
+        border-r-4 border-b-4 dark:bg-dark dark:border-light  dark:text-light"
         >
             <MovingImg
                 title={title}
                 img={img}
                 link={link}
             />
-            <span className="text-primary font-semibold pl-4">{date}</span>
+            <span className="text-primary font-semibold pl-4 ">{date}</span>
         </motion.li>
     )
 }
 
 const FeaturedArticles = ({ img, title, time, summary, link }) => {
     return (
-        <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+        <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light">
             <div
                 className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
-          rounded-br-3xl"
+          rounded-br-3xl dark:bg-light"
             />
             <Link
                 href={link}
                 target="_blank"
                 className=" w-full inline-block cursor-pointer overflow-hidden
-              rounded-lg "
+              rounded-lg  "
             >
                 <FramerImage
                     src={img}
                     alt={title}
-                    className="w-full h-auto"
+                    className="w-full h-auto  "
                     whileHover={{ scale: 1.05 }}
                     transition={{ duration: 0.5 }}
                 />
@@ -100,13 +100,13 @@ const FeaturedArticles = ({ img, title, time, summary, link }) => {
                 href={link}
                 target="_blank"
             >
-                <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline">
+                <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline  dark:text-light ">
                     {' '}
                     {title}{' '}
                 </h2>
             </Link>
-            <p className="text-sm mb-2"> {summary} </p>
-            <span className="text-primary font-bold"> {time} </span>
+            <p className="text-sm mb-2 text-dark dark:text-light "> {summary} </p>
+            <span className="text-primary font-bold "> {time} </span>
         </li>
     )
 }
@@ -123,14 +123,14 @@ const articles = () => {
             </Head>
             <main
                 className="w-full mb-16 flex flex-col items-center
-            justify-center overflow-hidden"
+            justify-center overflow-hidden "
             >
-                <Layout classname="pt-16">
+                <Layout classname="pt-16 ">
                     <AnimatedText
-                        text="Quepassa gente "
-                        className="mb-16 "
+                        text="Articulos De Interes"
+                        className="mb-10 text-xl lg:text-4xl font-bold dark:text-light"
                     />
-                    <ul className="grid grid-cols-2 gap-16">
+                    <ul className="grid grid-cols-2 gap-16 ">
                         <FeaturedArticles
                             title="Build a custom pagination component in reactjs from scratch"
                             summary="learn how to build a custom pagination component in reactjs from scratch
@@ -149,10 +149,10 @@ const articles = () => {
                             img={article2}
                         />
                     </ul>
-                    <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">
+                    <h2 className="font-bold text-4xl w-full text-center my-16 mt-32 dark:text-light">
                         Todos los Articulos{' '}
                     </h2>
-                    <ul>
+                    <ul className=''>
                         <Article
                             title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
                             date="una fecha aleotoria"
