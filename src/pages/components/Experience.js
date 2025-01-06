0254b8fbc6e5a485f3754999f70119bb6230bf89
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-05 22:55:10 -0300

    Refactor Experience component for improved readability and maintainability; update Footer and HireMe components for better styling and responsiveness

--- src/pages/components/Experience.js
+++ src/pages/components/Experience.js
@@ -1,66 +1,67 @@
-import React, { useRef } from 'react'
-import { useScroll } from 'framer-motion'
-import { motion } from 'framer-motion'
-import LilIcon from './LilIcon'
+import React, { useRef } from "react";
+import { useScroll } from "framer-motion";
+import { motion } from "framer-motion";
+import LilIcon from "./LilIcon";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
-  const ref  = useRef(null);
-  return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto  flex flex-col items-center
-  justify-between md:w-[80%]'>
-
-      <LilIcon reference={ref}/>
-
-    <motion.div
-    initial={{y:50}}
-    whileInView={{y:0}}
-    transition={{duration:0.5, type: 'spring'}}
-  
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto  flex flex-col items-center
+  justify-between md:w-[80%]"
     >
-      <h3 className='capitalize font-bold  text-2xl sm:text-xl xs:text-lg'>
-      {position}&nbsp; <a href={companyLink}
-        target={"_blank"}
-        className='text-red-700 dark:text-primaryDark capitalize'
-      >{company}</a></h3>
-      <span className='capitalize font-medium text-blue-600 dark:text-light/75 xs:text-sm'>
-        {time}|{address}
-      </span>
-      <p className='font-medium w-full md:text-sm'>
-        {work}
-      </p>
-    </motion.div>
-  </li>
-}
+      <LilIcon reference={ref} />
 
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold  text-2xl sm:text-xl xs:text-lg">
+          {position}&nbsp;{" "}
+          <a
+            href={companyLink}
+            target={"_blank"}
+            className="text-red-700 dark:text-primaryDark capitalize"
+          >
+            {company}
+          </a>
+        </h3>
+        <span className="capitalize font-medium text-blue-600 dark:text-light/75 xs:text-sm">
+          {time}|{address}
+        </span>
+        <p className="font-medium w-full md:text-sm">{work}</p>
+      </motion.div>
+    </li>
+  );
+};
 
 const Experience = () => {
-const ref  = useRef(null);
-  const { scrollYProgress } = useScroll(
-    {
-      
-     target:ref,
-     offset: ["start end", "center start"]
-    }
-  );
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
 
   return (
-    <div className='my-64'>
-      <h2 className='font-bold text-5xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16 '>
+    <div className="my-64">
+      <h2 className="font-bold text-5xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16 ">
         Experiencia
       </h2>
-      <div ref={ref} className='w-[75%] m-auto relative lg:w-[90%] md:w-full'>
-
-          <motion.div
-          style={{scaleY: scrollYProgress}}
-           className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
+      <div ref={ref} className="w-[75%] m-auto relative lg:w-[90%] md:w-full">
+        <motion.div
+          style={{ scaleY: scrollYProgress }}
+          className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
             md:w-[2px] md:left-[30px] xs:left-[20px]
            
-           '/>
-        <ul  className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
+           "
+        />
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
-
             position="Agente de viajes"
             companyLink="Multiviajes"
-            // time="2018-2019 "   
+            // time="2018-2019 "
             address=" Santiago | Calle Ahumada "
             work="Experiencia de ventas de paquetes turísticos,
             centrada en abordar las necesidades integrales
@@ -70,22 +71,7 @@ const ref  = useRef(null);
             "
           />
           <Details
-
-            position="Promotor turístico (Travél Rock)"
-            companyLink="www.google.com"
-            // time=" 2019-2020 "
-            address=" Santiago | Providencia"
-            work="Promotor enfocado en giras de viajes a Bariloche
-            para residencias estudiantiles. Presentando
-            actividades, opciones de alojamiento, seguros y
-            rutas. Enfoque en proporcionar experiencias
-            personalizadas.
-            "
-          />
-        
-          <Details
-
-            position="Operador de turismo(Rutacruz)-Practica profesional"
+            position="Operador de turismo(Rutacruz)"
             companyLink="www.google.com,"
             // time=" 2020-2021 "
             address=" Santiago | San Antonio 65"
@@ -95,9 +81,8 @@ const ref  = useRef(null);
             Propuesta de nuevos productos
             Apoyo en gestión de Tours en Santiago
             "
-                      />
-                        <Details
-
+          />
+          <Details
             position="Staff (Turistik)"
             companyLink="www.google.com"
             // time=" 2022-2023 "
@@ -108,23 +93,19 @@ const ref  = useRef(null);
             Especializado en operaciones específicas como
             teleférico, funicular .
             "
-            />
-                      {/* <Details
-
-            position="Recepcionista (Hostel Cienfuegos)"
+          />
+          <Details
+            position="Desarrollador Freelance"
             companyLink="www.google.com"
-            time=" 2023-2023 "
-            address=" Santiago | CienFuegos 151"
-            work="Registro de llegadas y la gestión de
-            la reserva y check-out.
-            Registro de transacciones financiaras
-            incluyendo facturación y procesamiento
-            de pagos."
-          />   */}
+            // time=" 2019-2020 "
+            // address=" Santiago | Providencia"
+            work="Soy un desarrollador freelance autodidacta con experiencia en la creación de sitios web completos. Me especializo en diseñar interfaces intuitivas y optimizadas, garantizando tiempos de carga rápidos y un alto rendimiento. Utilizo diversas tecnologías para desarrollar proyectos personalizados que sean funcionales, atractivos y fáciles de navegar, siempre adaptándome a las necesidades específicas de cada cliente.
+            "
+          />
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -8,11 +8,9 @@ const Footer = () => {
    font-medium text-lg dark:text-light dark:border-light sm:text-base
   
    '>
-    <Layout classname="py-2 sm:py-6 flex items-center justify-between lg:flex-col lg:py-6 ">
+    <Layout classname="py-2   flex justify-center sm:py-6 items-center  lg:flex-col lg:py-6 ">
         <span>{new Date().getFullYear()} &copy; Todos los Derechos Reservados.</span>
 
-        <Link href="https://academia.holamundo.io/" target={"_blank"} className='underline
-         underline-offset-4' >Hola Mundo!&#9825;</Link>
     </Layout>
    </footer>
   )
