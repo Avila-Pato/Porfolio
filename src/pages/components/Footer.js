02f21373c8e93d566cb02cddc71c59eb1f662777
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-10 22:24:34 -0300

    Refactor Footer and HireMe components for improved readability and consistency

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import Layout from './Layout'
-import Link from 'next/link'
+import React from "react";
+import Layout from "./Layout";
 
 const Footer = () => {
   return (
-   <footer className='w-full border-t-2 border-solid border-dark
+    <footer
+      className="w-full border-t-2  border-solid border-dark
    font-medium text-lg dark:text-light dark:border-light sm:text-base
   
-   '>
-    <Layout classname="py-2   flex justify-center sm:py-6 items-center  lg:flex-col lg:py-6 ">
-        <span>{new Date().getFullYear()} &copy; Todos los Derechos Reservados.</span>
+   "
+    >
+      <Layout classname="py-2   flex justify-center  sm:py-6 items-center  lg:flex-col lg:py-6  ">
+        <span>
+          {new Date().getFullYear()} &copy; Todos los Derechos Reservados.
+        </span>
+      </Layout>
+    </footer>
+  );
+};
 
-    </Layout>
-   </footer>
-  )
-}
-
-export default Footer
+export default Footer;
--- src/pages/components/HireMe.js
+++ src/pages/components/HireMe.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { CircularText } from '../../lib/Icons';
+import React from "react";
+import { CircularText } from "../../lib/Icons";
 
 const HireMe = () => {
   return (
-    <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
+    <div
+      className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
     sm:right-0 z-50
-    '>
-      <div className='w-48 h-auto flex items-center justify-center relative md:w-24'>
-        <CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
+    "
+    >
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
+        <CircularText
+          className={"fill-dark animate-spin-slow dark:fill-light"}
+        />
 
-
-        <a href="https://wa.me/+56999912214" className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-white shadow-md border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-green-700 hover:text-dark 
+        <a
+          href="https://wa.me/+56999912214"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-white shadow-md border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-green-700 hover:text-dark 
         dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
         hover:dark:border-light
 
            md:w-12 md:h-12 md:text-[10px]
-        ' target={"_blank"} >
+        "
+          target={"_blank"}
+        >
           WhatsApp
         </a>
       </div>
