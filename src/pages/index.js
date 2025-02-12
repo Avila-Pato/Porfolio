c7978ef07172aa252f1b8d4ce868a1a3b3a5a6a7
Avila-Pato <p.avilaf1998@gmail.com>
2025-02-12 00:37:09 -0300

    Fix CV link and clean up layout in Home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,9 @@ export default function Home() {
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
+            <div></div>
             <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
+              {/* <h1>HOla</h1> */}
               <AnimatedText
                 text="Convirtiendo visión en realidad con código y diseño."
                 className="!text-4xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl my-4 font-medium"
@@ -73,7 +75,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/Cv Profesional-PatricioAvila.pdf"
+                  href="/Cv Profesional-PatricioAAFF.pdf.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
