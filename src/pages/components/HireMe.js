66d249d258b786b8f9e27edbf0e36ecb13fdb8e9
Avila-Pato <p.avilaf1998@gmail.com>
2025-04-28 12:01:28 -0400

    camviosCv

--- src/pages/components/HireMe.js
+++ src/pages/components/HireMe.js
@@ -14,7 +14,7 @@ const HireMe = () => {
         />
 
         <a
-          href="https://wa.me/+56999912214"
+          href="https://wa.me/+56968654247"
           className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-white shadow-md border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-green-700 hover:text-dark 
         dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
         hover:dark:border-light
