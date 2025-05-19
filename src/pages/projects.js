06345dd378c2b4cd7d8e58b8b3582c395b575c86
Avila-Pato <p.avilaf1998@gmail.com>
2025-05-19 18:19:17 -0400

    fix: Update GitHub link for featured project in projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -330,6 +330,7 @@ const projects = () => {
                 summary="Este proyecto consiste en el desarrollo de una página web diseñada para ofrecer interfaces intuitivas y atractivas, adaptadas a las necesidades específicas del cliente. La optimización del rendimiento es clave, garantizando tiempos de carga rápidos y un funcionamiento eficiente en todos los dispositivos. Utilizando Next.js como base tecnológica, cada sección de la web ha sido diseñada para ser funcional, visualmente impactante y alineada con los objetivos establecidos."
                 // github="https://github.com/Avila-Pato/Awwards-Website-Winning-ReactJs-Tailwind-CSS-GSAP"
                 link="https://rlcchile.com/"
+                github=" https://github.com/Avila-Pato/RLC"
                 type="Proyectos"
               />
             </div>
